bce98c35a1517378792c780892dd4ba040df1531
Roman Pidlipskiy <108235770+Randayy@users.noreply.github.com>
2024-12-31 06:23:05 +0200

    f

--- backend/app/services/anime_service.py
+++ backend/app/services/anime_service.py
@@ -198,6 +198,7 @@ class AnimeService:
                     }
                     animes.append(transformed_anime)
                 logger.info(f"Page {page_num} fetched")
+                print(f"Page {page_num} fetched")
                 time.sleep(0.5)
                 return animes        
         
@@ -206,7 +207,7 @@ class AnimeService:
             return None
     
     async def save_anime_list_in_db(self):
-        for x in range(1, 2):
+        for x in range(1, 10):
             animes = await self.parse_page_animes(x)
             if animes:
                 logger.info(f"Animes found: {animes[0]['anime_id']}")
